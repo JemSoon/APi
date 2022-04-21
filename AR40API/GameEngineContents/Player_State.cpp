12346212d36362885fc4e321766430929bb59b77
잼순 김 <highyeana@naver.com>
2022-04-22 01:01:35 +0900

    방향키에서 손을떼도 가속도가 유지되는걸 MoveDir=0.0f로 초기화

--- AR40API/GameEngineContents/Player_State.cpp
+++ AR40API/GameEngineContents/Player_State.cpp
@@ -85,12 +85,13 @@ void Player::MoveUpdate()
 			MoveDir += float4::RIGHT * GameEngineTime::GetDeltaTime() * AccSpeed_;
 			PlayerDir_ = float4::RIGHT;//총알 발사 방향 설정용
 		}
-		//else if (true == GameEngineInput::GetInst()->IsUp("Move Right"))
-		//{	//버튼 떼면 idle애니메이션으로
-		//	PlayerAnimationRender->ChangeAnimation("idle-R");
-		//	ChangeState(PlayerState::Idle);
-		//	return;
-		//}
+		else if (true == GameEngineInput::GetInst()->IsUp("Move Right"))
+		{	//버튼 떼면 idle애니메이션으로
+			PlayerAnimationRender->ChangeAnimation("idle-R");
+			ChangeState(PlayerState::Idle);
+			MoveDir.x = 0.0f;
+			return;
+		}
 	}
 
 	{	//왼쪽
@@ -100,12 +101,13 @@ void Player::MoveUpdate()
 			MoveDir += float4::LEFT * GameEngineTime::GetDeltaTime() * AccSpeed_;
 			PlayerDir_ = float4::LEFT;
 		}
-		//else if (true == GameEngineInput::GetInst()->IsUp("Move Left"))
-		//{
-		//	PlayerAnimationRender->ChangeAnimation("idle-L");
-		//	ChangeState(PlayerState::Idle);
-		//	return;
-		//}
+		else if (true == GameEngineInput::GetInst()->IsUp("Move Left"))
+		{
+			PlayerAnimationRender->ChangeAnimation("idle-L");
+			ChangeState(PlayerState::Idle);
+			MoveDir.x = 0.0f;
+			return;
+		}
 	}
 
 	MoveDir += float4::DOWN * GameEngineTime::GetDeltaTime() * AccSpeed_;
@@ -124,15 +126,12 @@ void Player::MoveUpdate()
 		{
 			MoveDir.y = 0.0f;
 
-			NextPos_ = GetPosition() + (MoveDir * GameEngineTime::GetDeltaTime() * Speed_);
-			//그 때 발바닥 위치
-			float4 CheckPos1 = NextPos_ + float4(0.0f, 32.0f);
-			int Color1 = MapColImage_->GetImagePixel(CheckPos1);//갈수 있냐 없냐 색 체크
+			FootCheck();
 
-			if (RGB(255, 0, 0) != Color1 &&
-				RGB(55, 55, 55) != Color1 &&
-				RGB(0, 255, 255) != Color1 &&
-				RGB(0, 255, 0) != Color1)
+			if (RGB(255, 0, 0) != Color_ &&
+				RGB(55, 55, 55) != Color_ &&
+				RGB(0, 255, 255) != Color_ &&
+				RGB(0, 255, 0) != Color_)
 			{
 				SetMove(MoveDir * GameEngineTime::GetDeltaTime() * Speed_);
 			}
