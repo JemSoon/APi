1bb86cc8bcb717a41254dff17d96b6f2da557bb0
잼순 김 <highyeana@naver.com>
2022-05-05 08:11:57 +0900

    사운드 추가전 경로 정리(백업용)

--- AR40API/GameEngineContents/SuperMario.cpp
+++ AR40API/GameEngineContents/SuperMario.cpp
@@ -11,6 +11,7 @@
 #include <GameEngineBase/GameEngineFile.h>
 #include <GameEngine/GameEngineImageManager.h>
 #include <GameEngineBase/GameEngineInput.h>
+#include <GameEngineBase/GameEngineSound.h>
 
 SuperMario::SuperMario()
 {
@@ -27,22 +28,23 @@ void SuperMario::GameInit()
 	//창에 띄울 이미지 영역 사이즈
 	GameEngineWindow::GetInst().SetWindowScaleAndPosition({860,120}, {1280, 960});
 	{
-		//리소스 경로
-		GameEngineDirectory ResourcesDir;//생성과 동시에 현재 디렉토리
-		ResourcesDir.MoveParent("AR40API");//어디까지 상위 폴더로 올라가느냐
-		ResourcesDir.Move("Resources");//그다음 리소스 폴더로 이동
+		{	//리소스 경로
+			GameEngineDirectory ResourcesDir;//생성과 동시에 현재 디렉토리
+			ResourcesDir.MoveParent("AR40API");//어디까지 상위 폴더로 올라가느냐
+			ResourcesDir.Move("Resources");//그다음 리소스 폴더로 이동
+			ResourcesDir.Move("Image");
 
-		//(참고)자식 경로까지는 찾지 않는다
-		//나중에 폴더 따로 분류하면 디렉토리 따로 또 만들어줘야 한다
-		//폴더안에 모든 이미지 파일을 찾는다
-		std::vector<GameEngineFile> AllImageFileList = ResourcesDir.GetAllFile("Bmp");
+			//(참고)자식 경로까지는 찾지 않는다
+			//나중에 폴더 따로 분류하면 디렉토리 따로 또 만들어줘야 한다
+			//폴더안에 모든 이미지 파일을 찾는다
+			std::vector<GameEngineFile> AllImageFileList = ResourcesDir.GetAllFile("Bmp");
 
-		for (size_t i = 0; i < AllImageFileList.size(); i++)
-		{
-			GameEngineImageManager::GetInst()->Load(AllImageFileList[i].GetFullPath());
+			for (size_t i = 0; i < AllImageFileList.size(); i++)
+			{
+				GameEngineImageManager::GetInst()->Load(AllImageFileList[i].GetFullPath());
 
+			}
 		}
-
 		{
 			//이미지를 찾아
 			GameEngineImage* Image0 = GameEngineImageManager::GetInst()->Find("walk-R.bmp");
