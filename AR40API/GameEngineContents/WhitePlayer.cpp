1f34923a1ff5f6544f3f25a3289cc81c58b7afba
잼순 김 <highyeana@naver.com>
2022-05-08 22:19:47 +0900

    흰마리오 카메라 반경 작아서 수정

--- AR40API/GameEngineContents/WhitePlayer.cpp
+++ AR40API/GameEngineContents/WhitePlayer.cpp
@@ -112,7 +112,7 @@ void WhitePlayer::Start()
 {
 	SetScale({ 64,128 });
 
-	WhitePlayerCameraCollision = CreateCollision("PlayerCamera", { 1600, 1280 }, { 200, -50 });
+	WhitePlayerCameraCollision = CreateCollision("PlayerCamera", { 1600, 2048 }, { 200, -50 });
 
 	WhitePlayerHeadHitCollision = CreateCollision("WhitePlayerHeadHit", { 1, 2 }, { 0,-65 });//박스 충돌용(1개만 충돌하게끔)
 	WhitePlayerHeadCollision = CreateCollision("PlayerHead", { 64, 1 }, { 0,-64 });
